0975107ef3d2070f54d0cb2fb1d30ec34eab2ca9
Alchyr <isithalchyr@gmail.com>
2026-03-13 04:24:32 -0700

    remove prefixes from paths

--- content/CharacterModTemplate/CharModCode/Relics/CharModRelic.cs
+++ content/CharacterModTemplate/CharModCode/Relics/CharModRelic.cs
@@ -1,4 +1,5 @@
 ﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
 using BaseLib.Utils;
 using CharMod.CharModCode.Character;
 using CharMod.CharModCode.Extensions;
@@ -8,7 +9,7 @@ namespace CharMod.CharModCode.Relics;
 [Pool(typeof(CharModRelicPool))]
 public abstract class CharModRelic : CustomRelicModel
 {
-    public override string PackedIconPath => $"{Id.Entry.ToLowerInvariant()}.png".RelicImagePath();
-    protected override string PackedIconOutlinePath => $"{Id.Entry.ToLowerInvariant()}_outline.png".RelicImagePath();
-    protected override string BigIconPath => $"{Id.Entry.ToLowerInvariant()}.png".BigRelicImagePath();
+    public override string PackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".RelicImagePath();
+    protected override string PackedIconOutlinePath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_outline.png".RelicImagePath();
+    protected override string BigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigRelicImagePath();
 }
